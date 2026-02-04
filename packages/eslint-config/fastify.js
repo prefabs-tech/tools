ca5abe8c4a6ef49499f642978ca6f5be6e59c692
Ramesh <ramesh@dzangolab.com>
2026-02-04 18:20:43 +0545

    chore: update eslint config for fastify

--- packages/eslint-config/fastify.js
+++ packages/eslint-config/fastify.js
@@ -1,119 +1,139 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import nodePlugin from "eslint-plugin-n";
+import prettierPluginRecommened from "eslint-plugin-prettier/recommended";
+import promisePlugin from "eslint-plugin-promise";
+import unicornPlugin from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["coverage/", "dist/", "node_modules/", "eslint.config.js"],
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:n/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:unicorn/recommended",
-    "prettier",
-  ],
-  overrides: [
-    {
-      files: "**/*.spec.ts",
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: [
-    "@typescript-eslint",
-    "import",
-    "n",
-    "prettier",
-    "promise",
-    "unicorn",
-  ],
-  rules: {
-    curly: ["error", "all"],
-    "brace-style": ["error", "1tbs"],
-    "import/order": [
-      "error",
-      {
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
+    plugins: {
+      n: nodePlugin,
+      import: importPlugin,
+      promise: promisePlugin,
+      unicorn: unicornPlugin,
+    },
+    rules: {
+      // Base rules
+      curly: ["error", "all"],
+      "brace-style": ["error", "1tbs"],
+
+      // Node rules
+      ...nodePlugin.configs["flat/recommended-script"].rules,
+      "n/no-unsupported-features/es-syntax": [
+        "error",
+        { ignores: ["modules"] },
+      ],
+      "n/no-unpublished-import": [
+        "error",
+        {
+          allowModules: [
+            "@faker-js/faker",
+            "mercurius-codegen",
+            "query-string",
+          ],
         },
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          ["parent", "sibling"],
-          "index",
-          "object",
-          "type",
-        ],
-        "newlines-between": "always",
-      },
-    ],
-    "n/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }],
-    "n/no-unpublished-import": [
-      "error",
-      {
-        allowModules: ["@faker-js/faker", "mercurius-codegen", "query-string"],
-      },
-    ],
-    "prettier/prettier": "error",
-    "unicorn/filename-case": [
-      "error",
-      {
-        cases: {
-          camelCase: true,
-          snakeCase: true,
+      ],
+
+      // Import rules
+      ...importPlugin.configs.recommended.rules,
+      ...importPlugin.configs.typescript.rules,
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
         },
-      },
-    ],
-    "unicorn/import-style": [
-      "error",
-      {
-        styles: {
-          "node:path": {
-            named: true,
+      ],
+
+      // Unicorn rules
+      ...unicornPlugin.configs.recommended.rules,
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            camelCase: true,
+            snakeCase: true,
           },
         },
-      },
-    ],
-    "unicorn/numeric-separators-style": [
-      "error",
-      {
-        number: {
-          minimumDigits: 6,
-          groupLength: 3,
+      ],
+      "unicorn/import-style": [
+        "error",
+        {
+          styles: {
+            "node:path": {
+              named: true,
+            },
+          },
         },
-      },
-    ],
-    // [DU 2024-SEP-10]: Disabled the 'unicorn/prefer-structured-clone' rule, which recommends using 'structuredClone' 
-    // instead of 'JSON.parse(JSON.stringify(...))' (see: https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-structured-clone.md). 
-    // This may cause warnings when using 'JSON.parse(JSON.stringify(data))'. The reason for using this approach is unclear, 
-    // but it could potentially lead to issues for other developers. Further review is needed to determine if 'structuredClone' should be used instead.
-    "unicorn/prefer-structured-clone": "off",
-    "unicorn/prevent-abbreviations": [
-      "error",
-      {
-        allowList: {
-          db: true,
-          docs: true,
-          env: true,
-          err: true,
-          i: true,
-          param: true,
-          req: true,
-          res: true,
+      ],
+      "unicorn/numeric-separators-style": [
+        "error",
+        {
+          number: {
+            minimumDigits: 6,
+            groupLength: 3,
+          },
         },
+      ],
+      "unicorn/prefer-structured-clone": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          allowList: {
+            db: true,
+            docs: true,
+            env: true,
+            err: true,
+            i: true,
+            param: true,
+            req: true,
+            res: true,
+          },
+        },
+      ],
+    },
+    settings: {
+      node: {
+        tryExtensions: [".js", ".json", ".node", ".ts"],
       },
-    ],
-  },
-  settings: {
-    node: {
-      tryExtensions: [".js", ".json", ".node", ".ts"],
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
     },
   },
-};
+  {
+    files: ["**/*.spec.ts"],
+    // overrides for spec files if any (legacy file had empty overrides for this pattern)
+  },
+  prettierPluginRecommened,
+];
