fa91db99b81976d923a286f4a05248d31cbc5aea
Kabin Khand Thakuri <kabin@dzangolab.com>
2026-05-07 16:03:49 +0545

    feat: add separate utilities package with parse function (#48)

---
+++ packages/utilities/src/parse.ts
@@ -0,0 +1,26 @@
+export const parse = (
+  value: boolean | number | string | undefined,
+  fallback?: bigint | boolean | null | number | string,
+) => {
+  if (value === undefined) {
+    return fallback;
+  }
+
+  if (typeof value === "boolean") {
+    return value;
+  }
+
+  switch (typeof fallback) {
+    case "boolean": {
+      return !!JSON.parse(value as string);
+    }
+
+    case "number": {
+      return JSON.parse(value as string);
+    }
+
+    default: {
+      return value;
+    }
+  }
+};
---
+++ packages/utilities/vite.config.ts
@@ -0,0 +1,24 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(dirname(fileURLToPath(import.meta.url)), "src/index.ts"),
+        fileName: (format) => `PrefabsTechToolsUtilities.${format}.js`,
+        name: "PrefabsTechToolsUtilities",
+      },
+      target: "esnext",
+    },
+    resolve: {
+      alias: {
+        "@": new URL("src/", import.meta.url).pathname,
+      },
+    },
+  };
+});
